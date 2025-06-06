a9df07ebfb3e69a10b8adeb23e2b6d8686359194
Artibar <artibarsagade2000@gmail.com>
2025-06-06 20:30:39 +0530

    App.jsx
    add  App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
-import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import DashboardPage from "./pages/DashboardPage";
-import EventPage from "./pages/EventPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import EventForm from "./components/EventForm";
-import AnalyticsPage from "./components/AnalyticsPage";
+import { AuthProvider } from "./src/contexts/AuthContext";
+import Navbar from "./src/components/Navbar";
+import HomePage from "./src/pages/HomePage";
+import LoginPage from "./src/pages/LoginPage";
+import SignupPage from "./src/pages/SignupPage";
+import DashboardPage from "./src/pages/DashboardPage";
+import EventPage from "./src/pages/EventPage";
+import NotFoundPage from "./src/pages/NotFoundPage";
+import EventForm from "./src/components/EventForm";
+import AnalyticsPage from "./src/pages/AnalyticsPage";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/event/new" element={<EventForm />} />
-          <Route path="/event/:eventId" element={<EventPage />} />
-          <Route path="/event/:eventId/analytics" element={<AnalyticsPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <div style={{ minHeight: "100vh" }}>
+          <Navbar />
+          <main style={{ padding: "20px" }}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/event/new" element={<EventForm />} />
+              <Route path="/event/:eventId" element={<EventPage />} />
+              <Route path="/event/:eventId/analytics" element={<AnalyticsPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </main>
+        </div>
       </Router>
     </AuthProvider>
   );
